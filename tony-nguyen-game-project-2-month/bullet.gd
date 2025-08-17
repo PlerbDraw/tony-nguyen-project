e877e8620954f2944c1af48c77b0397849a5fc85
PlerbDraw <Plerbdraw@gmail.com>
2025-08-18 10:36:46 +1200

    finished shooting

--- tony-nguyen-game-project-2-month/bullet.gd
+++ tony-nguyen-game-project-2-month/bullet.gd
@@ -10,3 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += transform.x * SPEED * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	
