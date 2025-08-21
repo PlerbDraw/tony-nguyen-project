bbf20ad1820ee9e481f5046bfadfd252e5e52b32
PlerbDraw <Plerbdraw@gmail.com>
2025-08-21 19:09:30 +1200

    enemy ai

--- tony-nguyen-game-project-2-month/enemy.gd
+++ tony-nguyen-game-project-2-month/enemy.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
 
-var speed = 100 
+var speed = 50
+var player_chase = false 
+var player = null
 
+func _physics_process(delta): 
+	if player_chase: 
+		position += (player.position - position)/speed
+		
+		
 
 
 func _process(delta): 
@@ -14,3 +21,15 @@ func _process(delta):
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	print("dead")
 	queue_free()
+
+
+func _on_detection_area_body_entered(body):
+	player = body
+	player_chase = true 
+
+
+
+
+func _on_detection_area_body_exited(body): 
+	player = null 
+	player_chase = false
