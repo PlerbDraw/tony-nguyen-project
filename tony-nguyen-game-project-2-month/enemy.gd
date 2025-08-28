c66ae4f0e78f2d3ce47a6d7270fd028fab286f69
PlerbDraw <Plerbdraw@gmail.com>
2025-08-28 13:19:10 +1200

    Player character and enemies

--- tony-nguyen-game-project-2-month/enemy.gd
+++ tony-nguyen-game-project-2-month/enemy.gd
@@ -1,19 +1,33 @@
 extends CharacterBody2D
 
+var player: Node
+var speed: float = 200.0 
+var dead: bool = false
+var health: int = 3
+@export var health_ui = Node
 
-var speed = 50
+
+
 var player_chase = false 
-var player = null
 
-func _physics_process(delta): 
+
+func _process(_delta: float) -> void:
+	if not player == null:
+		look_at(player.global_position)
+	
+	velocity = Vector2(1, 0).rotated(rotation) * speed 
+	
+	move_and_slide()
+
+
+
+func _physics_process(_delta): 
 	if player_chase: 
 		position += (player.position - position)/speed
 		
 		
 
 
-func _process(delta): 
-	global_position.y += speed * delta 
 
 
 
@@ -27,9 +41,18 @@ func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true 
 
+func hit(): 
+	health -= 1 
+	health_ui.value = health 
+	health_ui.show()
+	if health <= 0:
+		dead = true
+		queue_free()
+	return dead
 
 
 
-func _on_detection_area_body_exited(body): 
+
+func _on_detection_area_body_exited(_body): 
 	player = null 
 	player_chase = false
--- tony-nguyen-game-project-2-month/player_scenes/game.gd
+++ tony-nguyen-game-project-2-month/player_scenes/game.gd
@@ -2,20 +2,62 @@ extends Node2D
 
 var enemy = preload("res://enemy.tscn")
 
-
-
+@export var enemy_scene: PackedScene
+@export var enemy_spawn: Node
+@export var wave_ui: Node
+@export var spawn_timer: Node
+@export var enemy_ui: Node
+#this is to export different assets and nodes into the game
+#allowing it to show different features like waves and health.
 
 
 var current_wave: int = 1
 var enemies_remaining: int 
 var enemies_spawned: int = 0 
 
-var
-var
-var 
-var enemy_spawn_time
-var enemy_health
-var enemy_speed
+var new_enemy_count: int = 3 
+var last_enemy_count: int = 2
+var wave_enemy_count:int = new_enemy_count + last_enemy_count
+var enemy_spawn_time: int = 4.0 
+var enemy_health: int = 3
+var enemy_speed: float = 200.0  
+
+
+func _ready() -> void:
+	wave_ui.text = "Wave: " + str(current_wave)
+	enemy_ui.text = "Enemies Remaining: " + str(wave_enemy_count)
+	enemies_remaining = wave_enemy_count
+	last_enemy_count = new_enemy_count
+	new_enemy_count = wave_enemy_count
+	#this is codes for the wave system that exists in the game. 
+
+func update_enemy_ui() -> void:
+	enemies_remaining -= 1 
+	enemy_ui.text = "Enemies Remaining: " + str(enemies_remaining)
+	if enemies_remaining <= 0: 
+		_new_wave()
+#this is ui text to deliver information about the enemies remaining to player
+
+func _new_wave() -> void:
+	current_wave += 1
+	if current_wave % 3 == 0: 
+		enemy_health
+		enemy_speed
+	if enemy_spawn_time > 1: 
+		enemy_spawn_time -= 0.3
+	enemies_spawned = 0
+	wave_enemy_count = last_enemy_count + new_enemy_count
+	last_enemy_count = new_enemy_count
+	new_enemy_count = wave_enemy_count
+	enemies_remaining = wave_enemy_count
+	wave_ui.text = "Wave: " + str(current_wave)
+	enemy_ui.text = "Enemies Remaining: " + str(wave_enemy_count)
+	spawn_timer.start()
+	#this tells us that when the new wave starts, enemies stats are modified
+	#enemies gets faster, stronger and spawn more frequently.
+
+
+
 
 
 
