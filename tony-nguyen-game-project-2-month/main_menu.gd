a8c593e33921f011c4795188bcc677191daa780d
PlerbDraw <Plerbdraw@gmail.com>
2025-10-07 12:26:08 +1300

    Menu work

--- tony-nguyen-game-project-2-month/main_menu.gd
+++ tony-nguyen-game-project-2-month/main_menu.gd
@@ -1,14 +1,27 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
+
+func _ready(): 
+	main_buttons.visible = true
+	options.visible = false
+
+
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://player_scenes/game.tscn")
 
 
 
 func _on_quit_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
 
 
-func _on_options_pressed() -> void:
-	pass # Replace with function body.
+func _on_options_pressed():
+	main_buttons.visible = false 
+	options.visible = true
+
+
+func _on_back_options_pressed() -> void:
+	_ready()
