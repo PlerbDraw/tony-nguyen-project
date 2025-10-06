b977f632f5df48de2633442d350ff88de958dffd
PlerbDraw <Plerbdraw@gmail.com>
2025-10-07 11:26:21 +1300

    Ugh

--- tony-nguyen-game-project-2-month/main_menu.gd
+++ tony-nguyen-game-project-2-month/main_menu.gd
@@ -1,8 +1,9 @@
 extends Control
 
 
-func _on_start_pressed() -> void:
-	pass # Replace with function body.
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
 
 
 func _on_quit_pressed() -> void:
